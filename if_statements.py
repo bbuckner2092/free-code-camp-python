53a534a3ad26b537bf399fa93d457c15b7fb3246
Brelon <bbuckner2092@gmail.com>
2023-08-15 14:33:09 -0400

    Completed if statement codes.

--- if_statements.py
+++ if_statements.py
@@ -1,7 +1,11 @@
-is_male = True
+is_male = False
 is_tall = True
 
-if is_male or is_tall:
-    print("You are a male or tall or both")
+if is_male and is_tall:
+    print('You are a tall male.')
+elif is_male and not(is_tall):
+    print("You are a short male.")
+elif not(is_male) and is_tall:
+    print("You are not a male but are tall.")
 else:
-    print("You are neither maloe or tall")
+    print('You are not male and not tall.')
